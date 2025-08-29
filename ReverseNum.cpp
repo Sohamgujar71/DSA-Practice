6083434c0d9fa8462b845057666439cdc6438c18
Sohamgujar71 <sohamgujar71@gmail.com>
2025-08-30 01:40:47 +0530

    Reveresed Digit

---
+++ ReverseNum.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void ReverseNum(int n){
+    int revNum=0;
+    while(n>0){
+        int lastDigit=n%10;
+        revNum=(revNum*10)+lastDigit;
+        n=n/10;
+    }
+    cout<<revNum;
+}
+
+
+int main(){
+int n;
+cin>>n;
+ReverseNum(n);
+
+}
