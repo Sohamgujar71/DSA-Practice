a1562905d4f443ec61124312f730a1b9461e113a
Sohamgujar71 <sohamgujar71@gmail.com>
2025-09-01 16:05:25 +0530

    recursion

---
+++ Recursion/PrintLinearly.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+ 
+void PrintName(int i,int n){
+
+    if(i>n){            //base case
+        return;
+    }
+    cout<<i<<" ";
+    PrintName(i+1,n);
+
+}
+int main(){
+    int n;
+    cin>>n;
+    PrintName(1,n);
+
+}
