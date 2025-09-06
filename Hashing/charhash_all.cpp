783454928cc2e3489e0faa36e10abbd0ce2942a2
Sohamgujar71 <sohamgujar71@gmail.com>
2025-09-06 21:36:04 +0530

    sorting

--- Hashing/charhash_all.cpp
+++ Hashing/charhash_all.cpp
@@ -8,12 +8,12 @@ int main(){
     cin>>s;
 
     //pre compute
-    int hash[26]={0};
+    int hash[256]={0};
     for(int i=0;i<s.size();i++){
-        hash[s[i]-'a']++;
+        hash[s[i]]++;
     }
     char c;
     cin>>c;
     //fetch
-    cout<<hash[c-'a'];
+    cout<<hash[c];
 }
